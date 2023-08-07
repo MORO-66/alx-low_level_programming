d07bced6b3d4cc6bd307f35e6198e5dce6dc18df
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-08-07 21:55:08 +0300

    asdf

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* create_array - function
+* @size: as you can see
+* @c: cahr
+*Return: string
+*/
+char *create_array(unsigned int size, char c)
+{
+if (size == 0)
+{
+return (NULL);
+}
+
+char *arr = (char *)malloc(size * sizeof(char));
+if (arr == NULL)
+{
+return (NULL);
+}
+
+int i;
+for (i = 0; i < size; i++)
+{
+arr[i] = c;
+}
+
+return (arr);
+}
+
