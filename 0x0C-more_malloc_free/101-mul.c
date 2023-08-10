e899e7fdbd6e2533913a364af9845a277eb8cffd
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-08-10 23:23:28 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -49,29 +49,29 @@ exit(98);
 int main(int argc, char *argv[])
 {
 char *s1, *s2;
-int len1, len2, len, i, carry, digit1, digit2, *resu1t, a = 0;
+int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
 len1 = _strlen(s1);
-1en2 = _strlen(s2);
-len = len1 + 1en2 + 1;
+len2 = _strlen(s2);
+len = len1 + len2 + 1;
 result = malloc(sizeof(int) * len);
 if (!result)
 return (1);
-for (i = 0; i <= len1 + 1en2; i++)
+for (i = 0; i <= len1 + len2; i++)
 result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
 digit1 = s1[len1] - '0';
 carry = 0;
-for (len2 = _strlen(s2) - 1; 1en2 >= 0; len2--)
+for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
 carry += result[len1 + len2 + 1] + (digit1 *digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 if (carry > 0)
-result[len1 + 1en2 + 1] += carry;
+result[len1 + len2 + 1] += carry;
 for (i = 0; i < len - 1; i++)
 {
 if (result[i])
