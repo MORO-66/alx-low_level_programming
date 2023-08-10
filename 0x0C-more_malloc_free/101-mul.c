fc92481c8396c58f98e2c5276326df775a82d8d2
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-08-10 23:16:42 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -62,15 +62,15 @@ return (1);
 for (i = 0; i <= len1 + 1en2; i++)
 result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
-digit1 = s1[len1] -'0';
+digit1 = s1[len1] - '0';
 carry = 0;
 for (len2 = _strlen(s2) - 1; 1en2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
+carry += result[len1 + len2 + 1] + (digit1 *digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
-If (carry > 0)
+if (carry > 0)
 result[len1 + 1en2 + 1] += carry;
 for (i = 0; i < len - 1; i++)
 {
