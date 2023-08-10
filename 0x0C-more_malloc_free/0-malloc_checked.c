df3d8e65d55a7c3101d0d6d19fbfc21f6d1021a3
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-08-10 20:50:01 +0300

    asdf

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+*malloc_checkedb - allocates memory using mall
+*@b: number of bytes to allocate
+* Return: a pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr== NULL)
+{
+exit(98);
+}
+return (ptr);
+}
