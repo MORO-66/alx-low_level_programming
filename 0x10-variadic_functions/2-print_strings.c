0033f81cb962b71b3709c49a1defedc4d71998fb
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-08-17 21:01:24 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,12 +15,12 @@ va_list args;
 char *str;
 if (!n)
 {
-printf("\n");  
+printf("\n");
 return;
 }
 va_start(args, n);
 while (i--)
 printf("%s%s", (str = va_arg(args, char *)) ? str : "(nil)",
-i? (separator ? separator: "") : "\n");
-va_end (args);
+i ? (separator ? separator : "") : "\n");
+va_end(args);
 }
