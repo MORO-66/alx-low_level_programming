7c5f8ff3538ca3975d4646dbf914830e2ed4a3b2
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-08-28 21:31:23 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,1 +1,38 @@
+#include "lists.h"
 
+/**
+ * delete_nodeint_at_index - add at beginning
+ * @head: frist node
+ * @index: index
+ * Return: int
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *tmp, *tmp1;
+unsigned int i = 0;
+if (!head || !*head)
+return (NULL);
+
+if (!index)
+{
+tmp = *head;
+*head = head->next;
+free(tmp);
+return (1);
+}
+tmp = *head;
+while (tmp)
+{
+if (i == idx )
+{
+tmp1->next = tmp->next;
+free(tmp);
+return (1);
+}
+i++;
+tmp1 = tmp;
+tmp = tmp->next;
+}
+return (-1);
+}
