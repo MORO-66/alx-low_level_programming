39db0b30a398e2d665ae778f1f30a3bf60d801ec
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-08-28 20:03:27 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,21 @@
+#include "lists.h"
 
+
+
+/**
+ * free_list - free
+ * @head: h
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+
+}
