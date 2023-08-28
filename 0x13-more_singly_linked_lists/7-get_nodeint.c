57e22f1b172c5aa77bae8a4701ab87a21bf4999a
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-08-28 20:39:35 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 listint_t *tmp;
 unsigned int i;
-temp = head
+temp = head;
 for (i = 0; node && i < index; n++;)
 node = node->next;
 return (node);
