c061c8612a53e9de098c9baab6dde8b9da04574f
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-08-28 21:34:35 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,19 +12,19 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 listint_t *tmp, *tmp1;
 unsigned int i = 0;
 if (!head || !*head)
-return (NULL);
+return (-1);
 
 if (!index)
 {
 tmp = *head;
-*head = head->next;
+*head = (*head)->next;
 free(tmp);
 return (1);
 }
 tmp = *head;
 while (tmp)
 {
-if (i == idx )
+if (i == index)
 {
 tmp1->next = tmp->next;
 free(tmp);
