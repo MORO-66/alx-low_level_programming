4c53f86a84754496fdcd52128cb50eafa7213f37
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-08-28 22:50:03 +0300

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -9,7 +9,7 @@ listint_t *find_listint_loop(listint_t *head)
 listint_t *ptr, *end;
 if (head == NULL)
 return (NULL);
-for (end == head->next; end != NULL; end = end->next)
+for (end = head->next; end != NULL; end = end->next)
 {
 if (end == end->next)
 return (end);
