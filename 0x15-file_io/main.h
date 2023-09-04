aec72ff24b1a9e687f9d75d3674aa2c69f09907c
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-09-04 17:35:41 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#degine READ_BUF_SIZE 1024
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
