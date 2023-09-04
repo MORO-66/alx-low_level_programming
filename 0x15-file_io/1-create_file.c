41770f21a5b21d414b9df5214a8057dca9b84226
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-09-04 17:23:26 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,19 +17,19 @@ return (i);
 
 
 /**
- *create_file - fun
- *
- *@text_content: name
- *
- *@filename: f
- * Return: int
- */
+*create_file - fun
+*
+*@text_content: name
+*
+*@filename: f
+* Return: int
+*/
 
 
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-ssize_t bytes = 0, len = strlen(text_content);
+ssize_t bytes = 0, len = _strlen(text_content);
 if (!filename)
 return (-1);
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
