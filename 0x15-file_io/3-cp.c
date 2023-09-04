652cb7b6aaac337378fb891f25eed266decd44e4
MORO-66 <129593336+MORO-66@users.noreply.github.com>
2023-09-04 18:13:50 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #define USAGE "Usage: cp file from file_to\n"
 #define ERR_NOREAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
-#define ERR NOCLOSE "Error: Can't close fd %d\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 /**
 *main - program
@@ -34,9 +34,9 @@ dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 from_fd = close(from_fd);
 to_fd = close(to_fd);
 if (from_fd)
-dprintf(STDERR_FILENO, from_fd), exit(100);
+dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 if (to_fd)
-dprintf(STDERR_FILENO, from_fd), exit(100);
+dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 
 return (EXIT_SUCCESS);
 }
